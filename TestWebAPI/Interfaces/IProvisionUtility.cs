1d2537eeddacd56cf2ea8e5963fe3c6261c74ffc
Usama Azhar <usama.azhar@bentley.com>
2018-03-30 05:59:07 +0500

    Add project files.

---
+++ TestWebAPI/Interfaces/IProvisionUtility.cs
@@ -0,0 +1,15 @@
+﻿using Stateful.Backend.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWebAPI.Interfaces
+{
+    interface IProvisionUtility
+    {
+        Task<bool> CheckIfBackendExists(string backendName);
+        Task ProvisionNewBackend(string backendName);
+        IStatefulBackend GetBackendReference(string backendName);
+    }
+}
